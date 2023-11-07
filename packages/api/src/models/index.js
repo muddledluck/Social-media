302f2e7afb1aee3ec8f0630349adab2a10355ff1
Oscar <oscar@superiorcsgroup.com>
2023-11-07 22:25:28 +0530

    removed js files

--- packages/api/src/models/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.UserModel = void 0;
-var user_template_1 = require("./user.template");
-exports.UserModel = user_template_1.default;
--- packages/ui-web/hooks/user/useUserWeb.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.useUserWeb = void 0;
-var useUserWeb = function (options, config) { };
-exports.useUserWeb = useUserWeb;
