b5ef1c1cfacc025d0e9ffa02d9535f568e105c67
Oscar <oscar@superiorcsgroup.com>
2023-11-09 09:56:08 +0530

    implementd useUserWeb hook

--- packages/ui-web/hooks/user/useUserWeb.tsx
+++ packages/ui-web/hooks/user/useUserWeb.tsx
@@ -1,7 +1,7 @@
 import { useLocalStorage } from "@/hooks/storage/useLocalStorage";
-import { UserOptions, useUser } from "@monorepo/ui-core/src/hooks/user/useUser";
 import { refreshAccessToken } from "../../helpers/auth/token";
 import { useRouter } from "next/router";
+import { useUser, UserOptions } from "@monorepo/ui-core/src/hooks/user/useUser";
 import { SWRConfiguration } from "swr";
 export const useUserWeb = (
   options?: Partial<UserOptions>,
